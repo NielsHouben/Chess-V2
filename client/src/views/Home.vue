f5d66067932b372150dd304d599ad2f637068951
Niels Houben <Niels Houben>
2021-02-07 21:22:18 +0100

    router added and slight refactoring

---
+++ client/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <router-link to="/Game">Play</router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
